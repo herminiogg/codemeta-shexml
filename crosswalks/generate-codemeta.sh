6747a5deacb75f738e86e2eca8cb2d91d6f1d4bf
Herminio García González <herminiogg@hotmail.com>
2025-05-05 14:15:18 +0200

    Checked crosswalks and updated the framing process

--- crosswalks/generate-codemeta.sh
+++ crosswalks/generate-codemeta.sh
@@ -7,9 +7,11 @@ fi
 
 # generates the non framed versions
 # for Maven and Github this is enough as there are not linked entities
-java -Dfile.encoding=UTF8 -jar generated/shexml.jar -m codemeta-maven.shexml -nu -f json-ld -o generated/codemeta-maven.json
-java -Dfile.encoding=UTF8 -jar generated/shexml.jar -m codemeta-github.shexml -nu -f json-ld -o generated/codemeta-github.json
+java -Dfile.encoding=UTF8 -jar generated/shexml.jar -m codemeta-maven.shexml -nu -f json-ld -o generated/codemeta-maven.jsonld
+java -Dfile.encoding=UTF8 -jar generated/shexml.jar -m codemeta-github.shexml -nu -f json-ld -o generated/codemeta-github.jsonld
 java -Dfile.encoding=UTF8 -jar generated/shexml.jar -m codemeta-zenodo.shexml -nu -f json-ld -o generated/codemeta-zenodo.jsonld
 
-# converts to the framed version for Zenodo's case
+# converts to the framed version
+groovy ../convertToFramedJsonLD.groovy generated/codemeta-maven.jsonld codemeta-context.jsonld generated/codemeta-maven.json
+groovy ../convertToFramedJsonLD.groovy generated/codemeta-github.jsonld codemeta-context.jsonld generated/codemeta-github.json
 groovy ../convertToFramedJsonLD.groovy generated/codemeta-zenodo.jsonld codemeta-context.jsonld generated/codemeta-zenodo.json
