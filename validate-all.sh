c4e89f1e01ad785da2e744a5dadb40c8a7671210
Herminio García González <herminiogg@hotmail.com>
2026-04-22 12:04:45 +0200

    Fixed broken link in the validation script

--- validate-all.sh
+++ validate-all.sh
@@ -7,7 +7,7 @@ echo "" > shaclCommandOutput.txt # Clears the SHACL command output file
 
 if [ ! -d apache-jena ]; then
     echo "Downloading Apache Jena..."
-    curl -L https://dlcdn.apache.org/jena/binaries/apache-jena-5.5.0.zip -o apache-jena.zip
+    curl -L https://archive.apache.org/dist/jena/binaries/apache-jena-5.5.0.zip -o apache-jena.zip
     unzip -q apache-jena.zip -d apache-jena
 fi
 
